25d2352cb7cc59a8a1ec8a7a21cbd48e5005912e
getch-hasan <hasan15-3112@diu.edu.bd>
2025-01-13 16:03:33 +0600

    new

--- src/components/layout/navbar/index.jsx
+++ src/components/layout/navbar/index.jsx
@@ -1,49 +1,107 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React, { useState } from 'react';
-
+import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
-  const [isDrawerOpen,setIsDrawerOpen]=useState(false)
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const pathName = usePathname();
   const navList = [
-    { name: "Home", href: "/" },
+    
+    { name: "Home", href: "/home" },
     { name: "Services", href: "/services" },
-    { name: "Portfolio", href: "/portfolio" },
-    { name: "Clients", href: "/clients" },
-    { name: "Career", href: "/career" },
+    { name: "Reviews", href: "/reviews" },
+
+    { name: "Contacts", href: "/contacts" },
+    { name: "About", href: "/about" },
+   
   ];
-  
+
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
+
   return (
-    <div className=' w-full z-10 bg-white'>
-      <nav className="py-3 flex container mx-auto justify-between items-center">
-        <div className='flex items-center gap-2'>
-          {/* Drawer Toggle Button for Small Devices */}
-          <button onClick={toggleDrawer} className='lg:hidden md:hidden text-xl'>
-          </button>
-          <img className='h-14 w-14 ml-4' src="/logo.png" alt="Logo" />
-        </div>
-        <div className="hidden md:flex gap-10">
+    <div className="w-full  fixed top-0 z-50 h-16 bg-[#f8eded]">
+      <nav className="py-3 flex container-custom mx-auto justify-between items-center font-openSans">
+        <Link href={"/"} className="flex items-center md:gap-4 gap-1 ">
+          <img
+            className="h-14 w-14 ml-4 rounded-full"
+            src="/logo.webp"
+            alt="Logo"
+          />
+          <h1 className="font-bubblegum text-2xl">Bites</h1>
+        </Link>
+        <button
+          onClick={toggleDrawer}
+          className="lg:hidden absolute right-4 text-xl"
+        >
+          <FaBars />
+        </button>
+        <div className="hidden lg:flex items-center gap-10">
           {navList.map((item, index) => (
             <Link
-              href={item?.href}
-              className={`text-sm leading-7 font-normal relative hover:border-none after:absolute after:w-0 after:h-[5px] after:bottom-0 after:bg-primary after:transition-all after:duration-200 after:ease-in-out after:rounded-full hover:after:w-full hover:after:left-0 ${pathName === item?.href
-                ? "after:w-full after:left-0"
-                : "after:left-1/2"
-                }`}
+              aria-label={item.name}
+              href={item.href}
               key={index}
+              className={`${
+                item.name !== "Contact"
+                  ? "font-semibold text-black font-poppins text-md leading-7 relative hover:border-none after:absolute after:w-0 after:h-[5px] after:-bottom-1 after:bg-[#79D802] after:transition-all after:duration-200 after:ease-in-out after:rounded-full hover:after:w-full hover:after:left-0"
+                  : "font-semibold text-white font-poppins text-md bg-[#79D802] py-2 rounded-full px-6"
+              }
+          ${
+            pathName === item.href
+              ? "after:w-full after:left-0 font-bold text-[#79D802] font-poppins"
+              : "after:left-1/2"
+          }`}
+              aria-labelledby="labeldiv"
             >
-              <button className="nav_link pb-2 leading-5 capitalize">
-                {item?.name}
-              </button>
+              {item.name}
+            </Link>
+          ))}
+          {/* <Link
+            aria-label="Reserve Table"
+            href="#reserve"
+            className="font-semibold font-poppins text-md bg-primary py-2 rounded-full px-6"
+          >
+            Reserve Table
+          </Link> */}
+        </div>
+      </nav>
+
+      <div
+        className={`fixed inset-0 z-40 flex lg:hidden ${
+          isDrawerOpen ? "opacity-100 visible" : "opacity-0 invisible "
+        } transition-opacity duration-700 ease-in-out`}
+      >
+        {/* Overlay */}
+        <div className="fixed inset-0 " onClick={toggleDrawer}></div>
+        {/* Drawer */}
+        <div
+          className={`absolute top-0 right-0 bg-white w-64 h-full shadow-lg flex flex-col p-4 font-poppins transform transition-transform duration-700 ease-in-out ${
+            isDrawerOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <button onClick={toggleDrawer} className="self-end mb-4 mt-2">
+            <FaTimes size={24} />
+          </button>
+          {navList.map((item, index) => (
+            <Link
+              aria-label={item.name}
+              href={item.href}
+              key={index}
+              onClick={toggleDrawer}
+              className={`${
+                pathName === item.href
+                  ? "font-extrabold text-primary"
+                  : "text-black"
+              } text-lg py-2`}
+            >
+              {item.name}
             </Link>
           ))}
         </div>
-       
-      </nav>
+      </div>
     </div>
   );
 };
